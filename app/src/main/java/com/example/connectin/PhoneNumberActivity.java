f69ff7dba2646722e1138ee2f0ac02d6f398a26a
Anurag Sharma <sharmaanurag1910@gmail.com>
2022-03-10 22:39:15 +0530

    Chatting App

--- app/src/main/java/com/example/connectin/PhoneNumberActivity.java
+++ app/src/main/java/com/example/connectin/PhoneNumberActivity.java
@@ -2,16 +2,32 @@ package com.example.connectin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.connectin.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
 
 
 
